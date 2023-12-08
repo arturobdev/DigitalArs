9ffacefea77ce01afb9cbb36f2533dd437c9c1e1
Arturo Blanco <blancoarturo92@gmail.com>
2023-12-08 19:21:52 -0300

    accounts features are added

--- DigitalArs-copia/DigitalArs-copia/Controllers/AccountController.cs
+++ DigitalArs-copia/DigitalArs-copia/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using DigitalArs_copia.DTO_s;
 using DigitalArs_copia.Entities;
+using DigitalArs_copia.Infraestructure;
 using DigitalArs_copia.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -21,39 +22,85 @@ namespace DigitalArs_copia.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll(int parameter = 0)
         {
-            var accounts = await _unitOfWork.AccountRepository.GetAllAccounts(parameter);
-            return Ok("Hola") ;
+            try
+            {
+                var accounts = await _unitOfWork.AccountRepository.GetAllAccounts(parameter);
+                return ResponseFactory.CreateSuccessResponse(200, accounts);
+            }
+            catch (Exception ex)
+            {
+                return ResponseFactory.CreateErrorResponse(500, "A surprise error happened, please try again.");
+            }
         }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id, int parameter = 0)
+        public async Task<IActionResult> GetById([FromRoute] int id, int parameter = 0)
         {
-            var account = await _unitOfWork.AccountRepository.GetById(id);
-            
-            return Ok(account);
+            try
+            {
+                var account = await _unitOfWork.AccountRepository.GetById(id);
+                return ResponseFactory.CreateSuccessResponse(200, account);
+            }
+            catch (Exception ex)
+            {
+                return ResponseFactory.CreateErrorResponse(500, "A surprise error happened, please try again.");
+            }
         }
 
         [HttpPost]
         public async Task<IActionResult> AddAcount(AccountDTO accountDTO)
         {
-            
-            return Ok(await _unitOfWork.AccountRepository.InsertAccount(accountDTO));
+            try
+            {
+                var response = await _unitOfWork.AccountRepository.InsertAccount(accountDTO);
+                if (response == true)
+                {
+                    await _unitOfWork.Complete();
+                    return ResponseFactory.CreateSuccessResponse(200, "Account was added.");
+                }
+                return ResponseFactory.CreateErrorResponse(400, "Error adding account, make sure that user exist.");
+            }
+            catch (Exception ex)
+            {
+                return ResponseFactory.CreateErrorResponse(500, "A suprise error happened.");
+            }
         }
-
         [HttpPut]
-        public async Task<IActionResult> UpdateAccount(int id, AccountDTO accountDTO, int parameter = 0)
+        public async Task<IActionResult> UpdateAccount([FromRoute] int id, AccountDTO accountDTO, int parameter = 0)
         {
-            var result = await _unitOfWork.AccountRepository.UpdateAccount(accountDTO,id,parameter);
-            ;
-            return Ok(result);
+            try
+            {
+                var result = await _unitOfWork.AccountRepository.UpdateAccount(accountDTO, id, parameter);
+                if (result == true)
+                {
+                    await _unitOfWork.Complete();
+                    return ResponseFactory.CreateSuccessResponse(200, "Account was successfuly edited.");
+                }
+                return ResponseFactory.CreateErrorResponse(400, "Error editing account, make the information is correct.");
+            }
+            catch (Exception ex)
+            {
+                return ResponseFactory.CreateErrorResponse(500, "Surprise error happened.");
+            }
+
         }
 
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAccount(int id, int parameter = 0)
+        public async Task<IActionResult> DeleteAccount([FromRoute] int id, int parameter = 0)
         {
-            var result = await _unitOfWork.AccountRepository.DeleteAccountById(id, parameter);
-          
-            return Ok(result);
+            try
+            {
+                var result = await _unitOfWork.AccountRepository.DeleteAccountById(id, parameter);
+                if (result == true)
+                {
+                    await _unitOfWork.Complete();
+                    return ResponseFactory.CreateSuccessResponse(200, "Succesful removal");
+                }
+                return ResponseFactory.CreateErrorResponse(400, "Error deleting account.");
+            }
+            catch (Exception ex)
+            {
+                return ResponseFactory.CreateErrorResponse(500, "Error deleting account.");
+            }
         }
     }
 }
