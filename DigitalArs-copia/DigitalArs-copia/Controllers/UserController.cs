8e82c197ca268fbbb7f5f38fb0c52b22db81c034
etchepared <88214684+etchepared@users.noreply.github.com>
2023-12-09 21:56:32 -0300

    feat add user routes

--- DigitalArs-copia/DigitalArs-copia/Controllers/UserController.cs
+++ DigitalArs-copia/DigitalArs-copia/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using DigitalArs_copia.Infraestructure;
+﻿using DigitalArs_copia.DTO_s;
+using DigitalArs_copia.Infraestructure;
 using DigitalArs_copia.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -38,6 +39,37 @@ namespace DigitalArs_copia.Controllers
         public async Task<IActionResult> GetById(int id, int parameter = 0)
         {
             return ResponseFactory.CreateSuccessResponse(200, await _unitOfWork.UserRepository.GetById(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InsertUser(UserRegisterDTO userRegisterDTO)
+        {
+            var result = await _unitOfWork.UserRepository.InsertUser(userRegisterDTO);
+            if(result)
+            {
+                await _unitOfWork.Complete();
+                return ResponseFactory.CreateSuccessResponse(200, "Success");
+            }
+            return ResponseFactory.CreateErrorResponse(400, "Error");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(UserRegisterDTO userRegisterDTO, int id, int parameter = 0)
+        {
+            var result = await _unitOfWork.UserRepository.UpdateUser(userRegisterDTO, id, parameter);
+            if(result)
+            {
+                await _unitOfWork.Complete();
+                return ResponseFactory.CreateSuccessResponse(200, "Success");
+            }
+            return ResponseFactory.CreateErrorResponse(400, "Error");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserById(int id, int parameter = 0)
+        {
+            await _unitOfWork.Complete();
+            return ResponseFactory.CreateSuccessResponse(200, await _unitOfWork.UserRepository.DeleteUserById(id, parameter));
         }
     }
 }
